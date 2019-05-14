5dfdc7ec93a66be1e84e7fd8b464456eaebb3934
LINGDONG\tianhongyu <tianhogyu@forwardx.com>
2019-05-14 16:00:06 +0800

    结构调整

--- tt-springboot-rabbitMq/src/test/java/com/hy/tt/TtSpringbootRabbitMqApplicationTests.java
+++ tt-springboot-rabbitMq/src/test/java/com/hy/tt/TtSpringbootRabbitMqApplicationTests.java
@@ -10,7 +10,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class TtSpringbootDemoApplicationTests {
+public class TtSpringbootRabbitMqApplicationTests {
 
     @Autowired
     private RabbitSender rabbitSender;
