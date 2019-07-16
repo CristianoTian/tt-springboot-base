3221a5f8e93284e50a7096c9a1c6dcc7523bb6b9
LINGDONG\tianhongyu <912992145@qq.com>
2019-07-16 15:41:34 +0800

    jwt + shiro

---
+++ tt-springboot-shardingjdbc/src/main/java/com/hy/tt/TtSpringbootShardingjdbcApplication.java
@@ -0,0 +1,13 @@
+package com.hy.tt.ttspringbootshardingjdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TtSpringbootShardingjdbcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TtSpringbootShardingjdbcApplication.class, args);
+    }
+
+}
