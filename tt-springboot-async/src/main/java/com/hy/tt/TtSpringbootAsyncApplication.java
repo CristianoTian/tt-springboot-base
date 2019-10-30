d160898ffbdc7309847f540256a39a89070cc6b4
LINGDONG\tianhongyu <912992145@qq.com>
2019-10-30 14:18:08 +0800

    demo --- async

---
+++ tt-springboot-async/src/main/java/com/hy/tt/TtSpringbootAsyncApplication.java
@@ -0,0 +1,13 @@
+package com.hy.tt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TtSpringbootAsyncApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TtSpringbootAsyncApplication.class, args);
+    }
+
+}
